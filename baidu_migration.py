cae2bf1c3f456b3e580285567501209ebe60ef91
timmyhuang <timmyhuang@tencent.com>
2020-03-03 12:29:22 +0800

    add province_city_code

--- baidu_migration.py
+++ baidu_migration.py
@@ -4,33 +4,25 @@ import pandas as pd
 import json
 from datetime import date, timedelta
 from progressbar import progressbar
+import os
 
-def baidu_migration_rank(target_region, result_region, id, move_type, move_date):
+def rank(target_region, result_region, id, move_type, move_date):
     url = f'''https://huiyan.baidu.com/migration/{result_region}rank.jsonp?dt={target_region}&id={id}&type=move_{move_type}&date={move_date.strftime('%Y%m%d')}'''
     df = pd.DataFrame(json.loads(requests.get(url).text[3:-1])['data']['list'])
     df['value'] /= 100
     return df
 
-with pd.ExcelWriter(f'migration_to_sz_{date.today()}.xlsx') as writer:
-    dt = date.today()
-    df = []
-    for i in progressbar(range(40)):
-        dt -= timedelta(days=1)
-        try:
-            dfi = baidu_migration_rank('city', 'province', '440300', 'in', dt)
-            dfi = dict(zip(dfi.province_name, dfi.value))
-            dfi['move_date'] = dt
-            df.append(dfi)
-        except Exception as e:
-            print(e)
-    df = pd.DataFrame(df)
-    df.to_excel(writer, sheet_name='province', index=False)
+def internalflowhistory(id, move_date):
+    url = f'''http://huiyan.baidu.com/migration/internalflowhistory.jsonp?dt=city&id={id}&date={move_date.strftime('%Y%m%d')}'''
+    return json.loads(requests.get(url).text[3:-1])['data']['list']
 
-    dt = date.today()
-    df = pd.DataFrame(columns=['city_name', 'province_name', 'value', 'move_date'])
-    for i in progressbar(range(40)):
-        dt -= timedelta(days=1)
-        dfi = baidu_migration_rank('city', 'city', '440300', 'in', dt)
-        dfi['move_date'] = dt
-        df = pd.concat([df,dfi], ignore_index=True)
-    df.to_excel(writer, sheet_name='city', index=False)
+def internalflowhistory_all():
+    move_date = date.today()
+    province_city = pd.read_csv('province_city_id.csv')
+    curves = []
+    for idx,row in progressbar(pd.read_csv('province_city_id.csv').iterrows()):
+        curve = pd.DataFrame(internalflowhistory(row.city_id, move_date), columns=['date_key','internal_flow'])
+        curve['province'] = row.province_name
+        curve['city_name'] = row.city_name
+        curves.append(curve)
+    return pd.concat(curves, ignore_index=True)
