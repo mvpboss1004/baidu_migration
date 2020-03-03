53db848333b66190a179731bc4c4e29fbea72159
timmyhuang <timmyhuang@tencent.com>
2020-03-03 14:28:14 +0800

    internal flow test OK

--- baidu_migration.py
+++ baidu_migration.py
@@ -20,9 +20,15 @@ def internalflowhistory_all():
     move_date = date.today()
     province_city = pd.read_csv('province_city_id.csv')
     curves = []
-    for idx,row in progressbar(pd.read_csv('province_city_id.csv').iterrows()):
-        curve = pd.DataFrame(internalflowhistory(row.city_id, move_date), columns=['date_key','internal_flow'])
+    for i in progressbar(range(len(province_city))):
+        row = province_city.iloc[i]
+        df = pd.DataFrame(internalflowhistory(row.city_id, move_date).items(), columns=['date_key','internal_flow']).sort_values(by='date_key')
+        curve = pd.DataFrame()
+        curve['date_af'] = df[df.date_key>='20200101'].date_key.values
+        curve['flow_af'] = df[df.date_key>='20200101'].internal_flow.values
+        curve['date_bf'] = df.iloc[:len(curve)].date_key.values
+        curve['flow_bf'] = df.iloc[:len(curve)].internal_flow.values
         curve['province'] = row.province_name
-        curve['city_name'] = row.city_name
-        curves.append(curve)
+        curve['city'] = row.city_name
+        curves.append(curve[['province', 'city', 'date_bf', 'flow_bf', 'date_af', 'flow_af']])
     return pd.concat(curves, ignore_index=True)
