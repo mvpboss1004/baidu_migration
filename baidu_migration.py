630ea4c6293247aafcaca0767d36d03d84c31835
timmyhuang <timmyhuang@tencent.com>
2020-03-04 15:24:42 +0800

    rename columns

--- baidu_migration.py
+++ baidu_migration.py
@@ -10,10 +10,39 @@ def rank(target_region, result_region, id, move_type, move_date):
     url = f'''https://huiyan.baidu.com/migration/{result_region}rank.jsonp?dt={target_region}&id={id}&type=move_{move_type}&date={move_date.strftime('%Y%m%d')}'''
     df = pd.DataFrame(json.loads(requests.get(url).text[3:-1])['data']['list'])
     df['value'] /= 100
-    return df
+    return df.rename(columns={'province_name':'result_province', 'city_name':'result_city'})
+
+def rank_all(target_region, result_region, move_type, move_date):
+    ranks = []
+    if target_region == 'province':
+        province = pd.read_csv('province_id.csv')
+        for i in progressbar(range(len(province))):
+            row = province.iloc[i]
+            try:
+                df = rank(target_region,result_region,row.province_id,move_type,move_date)
+            except Exception as e:
+                print(row)
+                print(e)
+            else:
+                df['target_province'] = row.province_name
+                ranks.append(df)
+    elif target_region == 'city':
+        province_city = pd.read_csv('province_city_id.csv')
+        for i in progressbar(range(len(province_city))):
+            row = province_city.iloc[i]
+            try:
+                df = rank(target_region,result_region,row.city_id,move_type,move_date)
+            except Exception as e:
+                print(row)
+                print(e)
+            else:
+                df['target_province'] = row.province_name
+                df['target_city'] = row.city_name
+                ranks.append(df)
+    return pd.concat(ranks, ignore_index=True)
 
 def historycurve(target_region, id, move_type):
-    url = '''http://huiyan.baidu.com/migration/historycurve.jsonp?dt={target_region}&id={id}&type=move_{move_type}'''
+    url = f'''http://huiyan.baidu.com/migration/historycurve.jsonp?dt={target_region}&id={id}&type=move_{move_type}'''
     return json.loads(requests.get(url).text[3:-1])['data']['list']
 
 def historycurve_all(target_region, move_type):
@@ -23,40 +52,39 @@ def historycurve_all(target_region, move_type):
         for i in progressbar(range(len(province))):
             row = province.iloc[i]
             try:
-                df = pd.DataFrame(historycurve(target_region,row.province_id,move_type).items(), columns=['date_key','historycurve']).sort_values(bye='date_key')
+                df = pd.DataFrame(historycurve(target_region,row.province_id,move_type).items(), columns=['date_key','historycurve']).sort_values(by='date_key')
             except Exception as e:
                 print(row)
-                pring(e)
+                print(e)
             else:
-                df['province'] = row.province_name
+                df['target_province'] = row.province_name
                 curves.append(df)
     elif target_region == 'city':
         province_city = pd.read_csv('province_city_id.csv')
         for i in progressbar(range(len(province_city))):
             row = province_city.iloc[i]
             try:
-                df = pd.DataFrame(historycurve(row.city_id,row.city_id,move_type).items(), columns=['date_key','historycurve']).sort_values(by='date_key')
+                df = pd.DataFrame(historycurve(target_region,row.city_id,move_type).items(), columns=['date_key','historycurve']).sort_values(by='date_key')
             except Exception as e:
                 print(row)
                 print(e)
             else:
-                df['province'] = row.province_name
-                df['city'] = row.city_name
+                df['target_province'] = row.province_name
+                df['target_city'] = row.city_name
                 curves.append(df)
     return pd.concat(curves, ignore_index=True)
 
-def internalflowhistory(id, move_date):
-    url = f'''http://huiyan.baidu.com/migration/internalflowhistory.jsonp?dt=city&id={id}&date={move_date.strftime('%Y%m%d')}'''
+def internalflowhistory(id):
+    url = f'''http://huiyan.baidu.com/migration/internalflowhistory.jsonp?dt=city&id={id}&date={date.today().strftime('%Y%m%d')}'''
     return json.loads(requests.get(url).text[3:-1])['data']['list']
 
 def internalflowhistory_all():
-    move_date = date.today()
     province_city = pd.read_csv('province_city_id.csv')
     curves = []
     for i in progressbar(range(len(province_city))):
         row = province_city.iloc[i]
         try:
-            df = pd.DataFrame(internalflowhistory(row.city_id, move_date).items(), columns=['date_key','internal_flow']).sort_values(by='date_key')
+            df = pd.DataFrame(internalflowhistory(row.city_id).items(), columns=['date_key','internal_flow']).sort_values(by='date_key')
         except Exception as e:
             print(row)
             print(e)
