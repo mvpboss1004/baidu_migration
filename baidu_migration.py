5ddee5d5115a6c27044ffe8b21f96b81a6bbc075
timmyhuang <timmyhuang@tencent.com>
2020-03-04 12:21:54 +0800

    add history_curve

--- baidu_migration.py
+++ baidu_migration.py
@@ -12,6 +12,39 @@ def rank(target_region, result_region, id, move_type, move_date):
     df['value'] /= 100
     return df
 
+def historycurve(target_region, id, move_type):
+    url = '''http://huiyan.baidu.com/migration/historycurve.jsonp?dt={target_region}&id={id}&type=move_{move_type}'''
+    return json.loads(requests.get(url).text[3:-1])['data']['list']
+
+def historycurve_all(target_region, move_type):
+    curves = []
+    if target_region == 'province':
+        province = pd.read_csv('province_id.csv')
+        for i in progressbar(range(len(province))):
+            row = province.iloc[i]
+            try:
+                df = pd.DataFrame(historycurve(target_region,row.province_id,move_type).items(), columns=['date_key','historycurve']).sort_values(bye='date_key')
+            except Exception as e:
+                print(row)
+                pring(e)
+            else:
+                df['province'] = row.province_name
+                curves.append(df)
+    elif target_region == 'city':
+        province_city = pd.read_csv('province_city_id.csv')
+        for i in progressbar(range(len(province_city))):
+            row = province_city.iloc[i]
+            try:
+                df = pd.DataFrame(historycurve(row.city_id,row.city_id,move_type).items(), columns=['date_key','historycurve']).sort_values(by='date_key')
+            except Exception as e:
+                print(row)
+                print(e)
+            else:
+                df['province'] = row.province_name
+                df['city'] = row.city_name
+                curves.append(df)
+    return pd.concat(curves, ignore_index=True)
+
 def internalflowhistory(id, move_date):
     url = f'''http://huiyan.baidu.com/migration/internalflowhistory.jsonp?dt=city&id={id}&date={move_date.strftime('%Y%m%d')}'''
     return json.loads(requests.get(url).text[3:-1])['data']['list']
